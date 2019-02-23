cc43769ecf4868ad5cd60b929026b2ac47c71bea
jenkins <m.astafev@pflb.ru>
2019-02-24 01:06:31 +0300

    test done

--- src/ClientSession.java
+++ src/ClientSession.java
@@ -1,3 +1,5 @@
+import com.sun.net.httpserver.HttpServer;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -76,7 +78,7 @@ public class ClientSession implements Runnable
     }
 
     //get identifier of client resource
-    private String getUTIFromHeader(String header)
+    private String getURIFromHeader(String header)
     {
         int from = header.indexOf(" ") + 1;
         int to = header.indexOf(" ",from);
@@ -85,5 +87,48 @@ public class ClientSession implements Runnable
         if(paramIndex != -1)
             uri = uri.substring(0, paramIndex);
         return DEF_FILES + uri;
+    }
+    //send answer to the client - http of chosen resource
+    //if nothing is chosen - send list of free resources
+    private int send (String url) throws IOException
+    {
+        InputStream strm = HttpServer.class.getResourceAsStream(url);
+        int code = (strm != null) ? 200 : 404;
+        String header = getHeader(code);
+        PrintStream answer = new PrintStream(out, true, "UTF-8");
+        answer.print(header);
+        if(code == 200)
+        {
+            int count = 0;
+            byte[] buffer = new byte[1024];
+            while((count = strm.read(buffer)) != -1)
+                out.write(buffer, 0, count);
+            strm.close();
+        }
+        return code;
+    }
+    //return http-header of answer
+    private String getHeader(int code)
+    {
+        StringBuilder buffer = new StringBuilder();
+        buffer.append("HTTP/1.1 " + code + " " + getAnswer(code) + "\n");
+        buffer.append("Date: " + new Date().toString() + "\n");
+        buffer.append("Accept-Ranges: none\n");
+        //buffer.append("Content-Type: " + contentType + "\n");
+        buffer.append("\n");
+        return buffer.toString();
+    }
+    //return comments to the code of result
+    private String getAnswer(int code)
+    {
+        switch (code)
+        {
+            case 200:
+                return "OK";
+            case 404:
+                return "Not Found";
+            default:
+                return "Internal Server Error";
+        }
     }
 }
